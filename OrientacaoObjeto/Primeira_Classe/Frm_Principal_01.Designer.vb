454b192d22f6b78d528f0362c7c4d5150b784212
João Azevedo <devjoaoazevedo@gmail.com>
2020-07-29 11:50:26 -0300

    Add my first program

---
+++ OrientacaoObjeto/Primeira_Classe/Frm_Principal_01.Designer.vb
@@ -0,0 +1,54 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frm_principal_01
+    Inherits System.Windows.Forms.Form
+
+    'Descartar substituições de formulário para limpar a lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Exigido pelo Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'OBSERVAÇÃO: o procedimento a seguir é exigido pelo Windows Form Designer
+    'Pode ser modificado usando o Windows Form Designer.  
+    'Não o modifique usando o editor de códigos.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Lbl_Principal = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'Lbl_Principal
+        '
+        Me.Lbl_Principal.AutoSize = True
+        Me.Lbl_Principal.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.Lbl_Principal.Font = New System.Drawing.Font("Microsoft Sans Serif", 32.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Lbl_Principal.Location = New System.Drawing.Point(12, 9)
+        Me.Lbl_Principal.Name = "Lbl_Principal"
+        Me.Lbl_Principal.Size = New System.Drawing.Size(130, 51)
+        Me.Lbl_Principal.TabIndex = 0
+        Me.Lbl_Principal.Text = "Texto" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10)
+        '
+        'frm_principal_01
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(605, 295)
+        Me.Controls.Add(Me.Lbl_Principal)
+        Me.Name = "frm_principal_01"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Lbl_Principal As Label
+End Class
---
+++ OrientacaoObjeto/Primeira_Classe/Frm_Principal_01.vb
@@ -0,0 +1,12 @@
+﻿Public Class frm_principal_01
+    Public Sub New()
+
+        ' Esta chamada é requerida pelo designer.
+        InitializeComponent()
+
+        ' Adicione qualquer inicialização após a chamada InitializeComponent().
+
+        Me.Text = "Primeira Classe"
+        Lbl_Principal.Text = "Hello World!"
+    End Sub
+End Class
