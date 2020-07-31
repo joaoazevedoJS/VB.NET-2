1c2e81266283cdcfa9d4ab5889848662c4beb253
João Azevedo <devjoaoazevedo@gmail.com>
2020-07-31 08:43:46 -0300

    ByteBank-2

--- OrientacaoObjeto/2 - ByteBank/Cliente.vb
+++ OrientacaoObjeto/2 - ByteBank/Cliente.vb
@@ -4,10 +4,67 @@ Namespace ByteBank
 
     Public Class Cliente
 
-        Public Nome As String
-        Public CPF As String
-        Public Profissao As String
-        Public Cidade As String
+#Region "PROPRIEDADES"
+
+        Private m_Nome As String
+        Public Property Nome As String
+            Get
+                Return m_Nome
+            End Get
+            Set(value As String)
+                m_Nome = value
+            End Set
+        End Property
+
+        Private m_CPF As String
+        Public Property CPF As String
+            Get
+                Return m_CPF
+            End Get
+            Set(value As String)
+                m_CPF = TestaCPF(value)
+            End Set
+        End Property
+
+        Public Property Profissao As String
+        Public Property Cidade As String
+
+#End Region
+
+#Region "FUNÇÕES ESPECIAIS"
+        Private Function TestaCPF(CPF As String) As String
+            Dim dadosArray() As String = {"11111111111", "22222222222", "33333333333", "44444444444",
+            "55555555555", "66666666666", "77777777777", "88888888888", "99999999999"}
+            Dim vResultado As Boolean = True
+            Dim i, x, n1, n2 As Integer
+            CPF = CPF.Trim
+            For i = 0 To dadosArray.Length - 1
+                If CPF.Length <> 11 Or dadosArray(i).Equals(CPF) Then
+                    vResultado = False
+                End If
+            Next
+            If vResultado = False Then
+                Return "00000000000"
+            Else
+                For x = 0 To 1
+                    n1 = 0
+                    For i = 0 To 8 + x
+                        n1 = n1 + Val(CPF.Substring(i, 1)) * (10 + x - i)
+                    Next
+                    n2 = 11 - (n1 - (Int(n1 / 11) * 11))
+                    If n2 = 10 Or n2 = 11 Then n2 = 0
+                    If n2 <> Val(CPF.Substring(9 + x, 1)) Then
+                        vResultado = False
+                        Exit For
+                    End If
+                Next
+                If vResultado = False Then
+                    Return "00000000000"
+                End If
+            End If
+            Return CPF
+        End Function
+#End Region
 
     End Class
 
--- OrientacaoObjeto/2 - ByteBank/ContaCorrente.vb
+++ OrientacaoObjeto/2 - ByteBank/ContaCorrente.vb
@@ -3,11 +3,45 @@ Imports _2___ByteBank.ByteBank
 
 Public Class ContaCorrente
 
-    Public Titular As Cliente
-    Public Agencia As Integer
-    Public Conta As Integer
-    Public Saldo As Double = 100
-    Public Extrato As String = ""
+#Region "PROPRIEDADES"
+    Private m_Extrato As String
+    Public Property Extrato As String
+        Get
+            Return m_Extrato
+        End Get
+        Set(value As String)
+            m_Extrato = value
+        End Set
+    End Property
+
+    Public Property Conta As Integer
+    Public Property Agencia As Integer
+
+    Public m_Titular As Cliente
+    Public Property Titular As Cliente
+        Get
+            Return m_Titular
+        End Get
+        Set(value As Cliente)
+            m_Titular = value
+        End Set
+    End Property
+
+    Private m_Saldo As Double = 100
+    Public Property Saldo As Double
+        Get
+            Return m_Saldo
+        End Get
+        Set(value As Double)
+            If value < 0 Then
+                m_Saldo = 0
+            Else
+                m_Saldo = value
+            End If
+        End Set
+    End Property
+
+#End Region
 
     Public Function Sacar(ValorSacar As Double) As Boolean
 
